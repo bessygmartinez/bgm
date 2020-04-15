e47a645616a5292cd3bace5b2dd2ed55010bf441
Bessy Gee <nakiwarai13@gmail.com>
2020-04-15 13:13:30 -0400

    Updates

---
+++ precache-manifest.80470023ae6eb27c8979307456c1bb67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "659048c813c39c89e92ba5ba00537fd3",
+    "url": "/bgm/index.html"
+  },
+  {
+    "revision": "9365d3b72d07e1f7cc35",
+    "url": "/bgm/static/css/main.dd31110c.chunk.css"
+  },
+  {
+    "revision": "df65a253cb477b1994fb",
+    "url": "/bgm/static/js/2.363e73ca.chunk.js"
+  },
+  {
+    "revision": "890b83c3b503af9a5630afd9ff112890",
+    "url": "/bgm/static/js/2.363e73ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9365d3b72d07e1f7cc35",
+    "url": "/bgm/static/js/main.f2ba146a.chunk.js"
+  },
+  {
+    "revision": "48a3be8087c0d4ddf62e",
+    "url": "/bgm/static/js/runtime-main.40bcecff.js"
+  },
+  {
+    "revision": "d959e21cc810d31d4ee1a1557486637b",
+    "url": "/bgm/static/media/logo.d959e21c.svg"
+  }
+]);

0d89c155684e4e92e5687f8fee0659924c00aee6
Bessy Gee <nakiwarai13@gmail.com>
2020-04-16 14:56:58 -0400

    added initial code for About Me section

---
+++ src/components/AboutMe/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import $ from "jquery";
+import "./aboutme.css";
+
+function AboutMe() {
+    return (
+        <div className="about-me-text center-align">
+                <h2>Hi! My name is Bessy.</h2>
+                <p>I'm a designer of things digital.</p>
+        </div>
+    )
+}
+
+export default AboutMe;

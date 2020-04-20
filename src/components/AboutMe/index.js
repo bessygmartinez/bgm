819b96c18f4b2c083d6f12c2247c345be297a4f8
Bessy Gee <nakiwarai13@gmail.com>
2020-04-20 16:24:41 -0400

    added copy for about me

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -1,12 +1,16 @@
 import React from "react";
-import $ from "jquery";
 import "./aboutme.css";
 
 function AboutMe() {
     return (
         <div className="about-me-text center-align">
-                <h2>Hi! My name is Bessy.</h2>
-                <p>I'm a designer of things digital.</p>
+                <h2>Hi, I'm Bessy!</h2>
+                <p>I'm from Miami, FL, but I design and build in the digital world.
+                    Earned my full stack web development certificate from the University of Miami, 
+                    but have been fascinated by software and computers since I first laid hands on one back in 1993.</p>
+                <p>My personal interests include pop culture, beer, live music, anime, and cosplay. I'm also
+                    a sucker for a really great showtune.
+                </p>
         </div>
     )
 }

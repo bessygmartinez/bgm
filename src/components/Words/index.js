cbe7a298cdf66567abf86aefc327603ba67f4f89
Bessy Gee <nakiwarai13@gmail.com>
2020-04-22 13:12:28 -0400

    created Words component

---
+++ src/components/Words/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Words() {
+    return (
+        <h1 className="center-align greeny-text">
+        <span className="word">Web &nbsp; Developer</span>
+        <span className="word">Graphic &nbsp; Designer</span>
+        <span className="word">Miami-based</span>
+      </h1>
+    )
+}
+
+export default Words;

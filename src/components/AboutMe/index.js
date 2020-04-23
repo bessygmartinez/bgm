d712de68680cc2aacddcaf96224e71d35aadf14b
Bessy Gee <nakiwarai13@gmail.com>
2020-04-23 16:51:17 -0400

    changed copy for about me

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -6,9 +6,8 @@ function AboutMe() {
         <div className="about-me-text hide-me center-align">
                 <h2>Hi, I'm Bessy!</h2>
                 <p>I'm from Miami, FL, but I design and build in the digital world.
-                    Earned my full stack web development certificate from the University of Miami, 
-                    but have been fascinated by software and computers since I first laid hands on one back in 1993.</p>
-                <p>My personal interests include pop culture, beer, live music, anime, and cosplay. I'm also
+                    Earned my full stack web development certificate from the University of Miami.</p>
+                <p>My personal interests include pop culture, live music, anime, and cosplay. I'm also
                     a sucker for a really great showtune.
                 </p>
         </div>

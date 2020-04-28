10c195b38c7dc4241b7fe783230250e7ac93f563
Bessy Gee <nakiwarai13@gmail.com>
2020-04-28 12:48:55 -0400

    changed resourceful div to designer, added copy, added developer div and copy with flipped cloud and new animation style

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -4,16 +4,34 @@ import cloud1 from "./cloud1.svg";
 
 function Skills() {
     return (
-        <div className="resourceful">
+        <div>
+        <div className="designer">
             <div className="cloud1-svg hide-me vibrate-3">
-                <img src={cloud1} alt="cloud1" className="cloud1"></img>
+                <img src={cloud1} alt="cloud" className="cloud1"></img>
             </div>
             <h3 className="hide-me">
-                Skills Go Here
+                Designer
             </h3>
             <p className="hide-me">
-               Blah blah blah blah.
+               I've been a Freelance Graphic Designer for over 15 years and value clean design models, straighforward
+               content format, and meaningful interactions. 
             </p>
+        </div>
+
+        <div className="developer">
+            <div className="cloud2-svg hide-me vibrate-2">
+                <img src={cloud1} alt="cloud" className="cloud1 flip-cloud"></img>
+            </div>
+            <h3 className="hide-me clearfix dev-heading">
+                Developer
+            </h3>
+            <p className="hide-me">
+                Coding and watching the code come to life in the browser is one of my favorite pastimes. Some of the languages
+                I use and continue to develop are HTML5, CSS3, JavaScript, and SQL. I can also work with React, JSX, jQuery,
+                Express Handlebars, Node, Express, MySQL, MongoDB, Passport, JWT, and am familiar with GitHub and Heroku.
+
+            </p>
+        </div>
         </div>
     )
 }

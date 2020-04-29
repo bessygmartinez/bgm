5aae2e0b4e417fa01a87e80a7f62c061cfbc1e04
Bessy Gee <nakiwarai13@gmail.com>
2020-04-28 20:01:48 -0400

    added hide-me to cloud svg, changed copy

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -7,7 +7,7 @@ function Skills() {
         <div>
         <div className="designer">
             <div className="cloud1-svg hide-me vibrate-3">
-                <img src={cloud1} alt="cloud" className="cloud1"></img>
+                <img src={cloud1} alt="cloud" className="cloud1 hide-me"></img>
             </div>
             <h3 className="hide-me">
                 Designer
@@ -19,20 +19,19 @@ function Skills() {
         </div>
 
         <div className="developer">
-            <div className="cloud2-svg hide-me vibrate-2">
-                <img src={cloud1} alt="cloud" className="cloud2 flip-cloud"></img>
+            <div className="cloud2-svg vibrate-2 hide-me">
+                <img src={cloud1} alt="cloud" className="cloud2 flip-cloud hide-me"></img>
             </div>
-            <h3 className="hide-me clearfix dev-heading">
+            <h3 className="clearfix dev-heading hide-me">
                 Developer
             </h3>
             <p className="hide-me">
-                Coding and watching the code come to life in the browser is one of my favorite pastimes. Some of the languages
+                I love to code and watch it come to life in the browser. Some of the languages
                 I use and continue to develop are HTML5, CSS3, JavaScript, and SQL. I can also work with React, JSX, jQuery,
                 Express Handlebars, Node, Express, MySQL, MongoDB, Passport, JWT, and am familiar with GitHub and Heroku.
-
             </p>
         </div>
-        </div>
+</div>
     )
 }
 

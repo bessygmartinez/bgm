7c2d9e35c23a696e71e1e5b9fce54ae9f6f8d1c9
Bessy Gee <nakiwarai13@gmail.com>
2020-04-30 13:46:02 -0400

    changed if statement for hide-me to - 200

--- src/App.js
+++ src/App.js
@@ -77,7 +77,7 @@ class App extends Component {
               let bottom_of_window = $(window).scrollTop() + $(window).height();
               
               /* If the object is completely visible in the window, fade it */
-              if( bottom_of_window > bottom_of_object - 300 ){
+              if( bottom_of_window > bottom_of_object - 200){
                   
                   $(this).animate({'opacity':'1'}, 350);                        
               }

19bdb043d182a0fed310a4084a71eb0fbdc7d78b
Bessy Gee <nakiwarai13@gmail.com>
2020-04-30 14:02:24 -0400

    added vibrate-2 to rain cloud again, added stop-flickering class to divs

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -46,15 +46,15 @@ function Skills() {
             </p>
         </div>
 
-        <div className="rain">
+        <div className="rain stop-flickering">
 
                     <div className="cloud4-svg hide-me">
-                    <img src={cloud1} alt="rain cloud" className="cloud2 hide-me"></img>
+                    <img src={cloud1} alt="rain cloud" className="cloud2 vibrate-2 hide-me"></img>
                     </div>
 
-                <div className="ac-animated-svg-icon ac-svg-animated ac-svg-shadow">
-				<div className="ac-animated-svg-icon-contents">
-				<div className="ac-animated-svg-cloudrain">
+                <div className="ac-animated-svg-icon ac-svg-animated ac-svg-shadow stop-flickering">
+				<div className="ac-animated-svg-icon-contents stop-flickering">
+				<div className="ac-animated-svg-cloudrain stop-flickering">
                 <img src={drop} alt="drop" className="svg-drop svg-drop-1"></img>
                 <img src={drop} alt="drop" className="svg-drop svg-drop-2"></img>
                 <img src={drop} alt="drop" className="svg-drop svg-drop-3"></img>

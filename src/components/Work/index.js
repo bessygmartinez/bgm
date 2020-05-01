2d0246b0e0902be62bc9ea219ee0843beeda59c2
Bessy Gee <nakiwarai13@gmail.com>
2020-05-01 13:10:45 -0400

    added placeholder for work

---
+++ src/components/Work/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./work.css";
+
+function Work () {
+    return (
+        <div>
+            <div className="work1">
+                <h2>Work goes here</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Work;

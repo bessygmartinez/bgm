f876e905498d4b854034d81195838576cf33bdf1
Bessy Gee <nakiwarai13@gmail.com>
2020-05-14 18:21:45 -0400

    added id for about-me section

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -3,7 +3,7 @@ import "./aboutme.css";
 
 function AboutMe() {
     return (
-        <div className="about-me-text hide-me center-align">
+        <div className="about-me-text hide-me center-align" id="about-me">
                 <h2>Hi, I'm Bessy!</h2>
                 <p> I'm from Miami, FL, but I design and build in the digital world, having earned a
                     full stack web development certification from the University of Miami and a degree in

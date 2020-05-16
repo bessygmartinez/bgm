23833eb3df75aaf44d05c91e3ff5033dbbedd459
Bessy Gee <nakiwarai13@gmail.com>
2020-05-16 10:43:54 -0400

    added constructor for state, added handleChange function and handleSubmit function, added name and onChange to form inputs

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,6 +2,27 @@ import React, { Component } from "react";
 import "./contact.css";
 
 class Contact extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            name: '',
+            email: '',
+            message: ''
+        }
+
+        this.handleChange = this.handleChange.bind(this)
+    }
+    
+    handleChange = e => {
+            this.setState({ [e.target.name]: e.target.value })
+        }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        const { name, email, message } = this.state;
+    }
 
 render()
  {
@@ -9,22 +30,38 @@ render()
   return (
       <div>
           <div className="row contact-div">
-              <form className="col s12 m12 l12">
+              <form 
+              className="col s12 m12 l12"
+              onSubmit={this.handleSubmit}>
                   <h3>Send a Message</h3>
                   <div className="row">
                       <div className="input-field col s6 m6 l6">
-                          <input id="first_name" type="text" className="validate"></input>
-                          <label htmlFor="first_name">Name</label>
+                          <input 
+                          id="name"
+                          name="name"
+                          type="text"
+                          className="validate"
+                          onChange={this.handleChange} />
+                          <label htmlFor="name">Name</label>
                       </div>
                       <div className="input-field col s6 m6 l6">
-                          <input id="email" type="email" className="validate"></input>
+                          <input
+                          id="email"
+                          name="email"
+                          type="email"
+                          className="validate"
+                          onChange={this.handleChange} />
                           <label htmlFor="email">Email</label>
                       </div>
                   </div>
                   <div className="row">
                       <div className="input-field col s12 m12 l12">
-                      <textarea id="text_area" className="materialize-textarea"></textarea>
-                        <label htmlFor="text_area">Your Message</label>
+                      <textarea
+                      id="message"
+                      name="message"
+                      className="materialize-textarea"
+                      onChange={this.handleChange} />
+                        <label htmlFor="message">Your Message</label>
                       </div>
                   </div>
                   <div className="row">

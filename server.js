464b6be6b3aa89fa7a09638c585d99cbd1f7c6c4
Bessy Gee <nakiwarai13@gmail.com>
2020-05-18 13:56:41 -0400

    added if statement for production

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post("/api/form", (req, res) => {
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  }
+
+app.post("/bgm/api/form", (req, res) => {
     nodemailer.createTestAccount((err, account) => {
         const htmlEmail = `
         <h3>Contact Details</h3>
